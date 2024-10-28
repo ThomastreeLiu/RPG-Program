7ac86141db723e2983f5080be7efff8b7a5f11d9
ThomastreeLiu <107929062+ThomastreeLiu@users.noreply.github.com>
2024-10-28 10:14:20 -0700

    RPG upload

--- RPG.java
+++ RPG.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
 public class RPG {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String again = "y";
+        while (again.equals("y")){
+            int process = 0;
+            
+        }
+    }
 }
