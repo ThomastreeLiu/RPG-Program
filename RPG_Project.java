f8f6b47f32de8f5321a20a2c32886b9ea183ecb5
ThomastreeLiu <thomastree@126.com>
2024-11-02 02:40:25 -0700

    upload(task1 available)

--- RPG_Project.java
+++ RPG_Project.java
@@ -13,97 +13,125 @@ public class RPG_Project {
 
 
     public static void task1(Scanner scan) {
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
+        String quit = "y";
+        if ((process%100)%10==3){
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
+                    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            System.out.println("You have done this task, you want to:\n" +
+                    "1.Redo this task(the process of this task will be restart)\n" +
+                    "2.quit\n"+"Make your choise in number.");
+            int choise = scan.nextInt();
+            if (choise==1)
+                process=process-3;
+            else
+                System.out.println("You will back to manu");
+            try {
+                Thread.sleep(500);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        while ((process%100)%10==0&&quit.equals("y")){
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
                 "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        System.out.println("1941, Europe.\n" +
-                "Nazi Germany invade most country in west europe. Now they aim their goal to British, \n" +
-                "but Royal air force and Royal navy block them in east coast of atlantic ocean. \n" +
-                "To invade British, Hitler make a plan: \n" +
-                "Operation Sea Lion");
+            System.out.println("1941, Europe.\n" +
+                    "Nazi Germany invade most country in west europe. Now they aim their goal to British, \n" +
+                    "but Royal air force and Royal navy block them in east coast of atlantic ocean. \n" +
+                    "To invade British, Hitler make a plan: \n" +
+                    "Operation Sea Lion");
 
-        System.out.println("Press any key");
-        anykey = scan.next();
+            System.out.println("Press any key");
+            anykey = scan.next();
+            if (anykey.equals("q")||anykey.equals("Q"))
+                quit="n";
+            else
+                process = process + 1;
+        }
+        while ((process%100)%10==1&&quit.equals("y")) {
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
+                    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            System.out.println("You are a pilot of Supermarine Walrus, as usual, you take off from sea to " +
+                    "scouting the german front coast");
+            System.out.println("\n" +
+                    "\n" +
+                    "                 `··``´   `                                                                     ´`  \n" +
+                    "                        `                                                                           \n" +
+                    "                                                                                                    \n" +
+                    "´ ` ``                                           `                                   `´´```  `    ``\n" +
+                    "   `´`¨`                                                                          `` `````          \n" +
+                    "      ´``´        ˆ:····´´¨…`¨¨´ `                                                         `        \n" +
+                    "       ¸·©LùCI¬‘¸´ `´´·˜·ˆ…’…ˆ¨ˆ·   ```                                                             \n" +
+                    "   ’       ´``` ’CV‰s½½¢òÌ‰sÌn3Y¨·i¦³¬“°;ˆ¸’‚ˆ˜¨´`´·…´´`´ ```                              ´        \n" +
+                    "               `´¸› …´  a’     …300±üOµµxOx6e4¾f5™©Í0£wL£££@r×–:…`   ```´·´¨´¨·´´                   \n" +
+                    "                 ·ˆ    `@;     ´’°t3ç™Sy©åe¢ãè2ôžó5õüü&&22ü&üüç©Yùµ£a0ç2w©©üüüüüçw©£©56‚´ º       ·°\n" +
+                    "                 l¸    ˜’  ¨ ``  ¨’“À‹¯o2®PFüš€pˆ…`´`’‚˜…¸…¸·…  …´·„O“¨´…3©4xõ54Ox¾4O54¾xµ¾‹        \n" +
+                    "                 Ì…    s      ´`  h³£&ù£ù¾xõ44Àg¥¸·`´       ```    ¨y````a            `             \n" +
+                    "                ´0     ™          ÷¹ÇÇUïŸO66ŸbÀÝv         `¨`  …<n¼2=    ú                          \n" +
+                    "       ¨…¨…¨     <   ` C  ``·    ·~s¿/!1+‡9È$Vpý`  `ä±¨   ´……/IrƒJCíí·  ¯÷                         `\n" +
+                    "       ´¨wª˜’³¨`*¨   `·@       ``´ª6ª”*+f¼ÄÚm§´       ´…¸ˆ±šûžPÀpø¶‰ÙÚn’~ºˆ……·                     `\n" +
+                    "             ·ˆ`O5¯„}l}½’```     za}%~!„yw|[–¼            …·´±9ñá¶ü©4…–tj§9ŸÝûäû                   `\n" +
+                    "                ¬“»i^*8ëåj¿¾xƒóDnnV!||/¦ü÷+”)J            ´‘%ú¾TµÎçwç   ½¨· ¨`                   ``·\n" +
+                    "            ´•w«©§ÿÓ9(»¿r—%ókûõ0ÀœÎVYTö¤5™9×?j’…´´       ’¸…saç5¾aÍ0»   V   `                `    ``\n" +
+                    "            ¨`³D¶ƒ°’*JzÏjºº²³‹*‰/>Ýç5àfh¶ŠâG¶€ãppÕêÛÔ8FUs×~º~«?i*/í)` ²Ìn˜´```                      \n" +
+                    "`´             ·¸suoct<L<ìíIoOŠñzCòÍ$µ5UbÒÓÒbññ¾3Íò‰uµkÏnnú4SDZëbñÓŸ¥DFýdýÓÒDá$øXC/´°´·             \n" +
+                    "`                b¢½‡=>w>iïtoIt¤s3CwêBWOÿñàkžžŸù½uJ½z1=‰uò¢±‡©t&š9?     ´ ´`´˜…·…°’¸…3ôx§Z          \n" +
+                    "´                ˜Yáÿï<î7ï=llƒjsò±Yùa4ôÎ‡vÇTy6ùü†°;›ˆ:°´`´`  ¨`óú³7aÒ²¨²·…‚                         \n" +
+                    "`                ¨“ªVíJÇëµº/¯»z%Cóz>¦º„“÷;’¸¨``    `´          ˜èÕ¥ó:’¹¹;˜~  ‚…’…‚´      `    ˜     \n" +
+                    "                    `````  ¨````````                                                   `            \n" +
+                    "                 ```   `   ¨````````                                                                \n" +
+                    "                           ´```   ```                                                               \n" +
+                    "                                    ´´´`````````  ```  ¨¨´´´´´´`` ``` `````    `                    \n" +
+                    "                                               ```     ´´·¨´       ```  ```                       ` \n" +
+                    "                                                        ```´´´¨`` ``   ` `     `                `` `\n" +
+                    "\n");
+            //https://www.asciiart.eu/image-to-ascii
+            System.out.println("Press any key");
+            anykey = scan.next();
+            if (anykey.equals("q")||anykey.equals("Q"))
+                quit="n";
+            else
+                process = process + 1;
+        }
 
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-        System.out.println("You are a pilot of Supermarine Walrus, as usual, you take off from sea to " +
-                "scouting the german front coast");
-        System.out.println("\n" +
-                "\n" +
-                "                 `··``´   `                                                                     ´`  \n" +
-                "                        `                                                                           \n" +
-                "                                                                                                    \n" +
-                "´ ` ``                                           `                                   `´´```  `    ``\n" +
-                "   `´`¨`                                                                          `` `````          \n" +
-                "      ´``´        ˆ:····´´¨…`¨¨´ `                                                         `        \n" +
-                "       ¸·©LùCI¬‘¸´ `´´·˜·ˆ…’…ˆ¨ˆ·   ```                                                             \n" +
-                "   ’       ´``` ’CV‰s½½¢òÌ‰sÌn3Y¨·i¦³¬“°;ˆ¸’‚ˆ˜¨´`´·…´´`´ ```                              ´        \n" +
-                "               `´¸› …´  a’     …300±üOµµxOx6e4¾f5™©Í0£wL£££@r×–:…`   ```´·´¨´¨·´´                   \n" +
-                "                 ·ˆ    `@;     ´’°t3ç™Sy©åe¢ãè2ôžó5õüü&&22ü&üüç©Yùµ£a0ç2w©©üüüüüçw©£©56‚´ º       ·°\n" +
-                "                 l¸    ˜’  ¨ ``  ¨’“À‹¯o2®PFüš€pˆ…`´`’‚˜…¸…¸·…  …´·„O“¨´…3©4xõ54Ox¾4O54¾xµ¾‹        \n" +
-                "                 Ì…    s      ´`  h³£&ù£ù¾xõ44Àg¥¸·`´       ```    ¨y````a            `             \n" +
-                "                ´0     ™          ÷¹ÇÇUïŸO66ŸbÀÝv         `¨`  …<n¼2=    ú                          \n" +
-                "       ¨…¨…¨     <   ` C  ``·    ·~s¿/!1+‡9È$Vpý`  `ä±¨   ´……/IrƒJCíí·  ¯÷                         `\n" +
-                "       ´¨wª˜’³¨`*¨   `·@       ``´ª6ª”*+f¼ÄÚm§´       ´…¸ˆ±šûžPÀpø¶‰ÙÚn’~ºˆ……·                     `\n" +
-                "             ·ˆ`O5¯„}l}½’```     za}%~!„yw|[–¼            …·´±9ñá¶ü©4…–tj§9ŸÝûäû                   `\n" +
-                "                ¬“»i^*8ëåj¿¾xƒóDnnV!||/¦ü÷+”)J            ´‘%ú¾TµÎçwç   ½¨· ¨`                   ``·\n" +
-                "            ´•w«©§ÿÓ9(»¿r—%ókûõ0ÀœÎVYTö¤5™9×?j’…´´       ’¸…saç5¾aÍ0»   V   `                `    ``\n" +
-                "            ¨`³D¶ƒ°’*JzÏjºº²³‹*‰/>Ýç5àfh¶ŠâG¶€ãppÕêÛÔ8FUs×~º~«?i*/í)` ²Ìn˜´```                      \n" +
-                "`´             ·¸suoct<L<ìíIoOŠñzCòÍ$µ5UbÒÓÒbññ¾3Íò‰uµkÏnnú4SDZëbñÓŸ¥DFýdýÓÒDá$øXC/´°´·             \n" +
-                "`                b¢½‡=>w>iïtoIt¤s3CwêBWOÿñàkžžŸù½uJ½z1=‰uò¢±‡©t&š9?     ´ ´`´˜…·…°’¸…3ôx§Z          \n" +
-                "´                ˜Yáÿï<î7ï=llƒjsò±Yùa4ôÎ‡vÇTy6ùü†°;›ˆ:°´`´`  ¨`óú³7aÒ²¨²·…‚                         \n" +
-                "`                ¨“ªVíJÇëµº/¯»z%Cóz>¦º„“÷;’¸¨``    `´          ˜èÕ¥ó:’¹¹;˜~  ‚…’…‚´      `    ˜     \n" +
-                "                    `````  ¨````````                                                   `            \n" +
-                "                 ```   `   ¨````````                                                                \n" +
-                "                           ´```   ```                                                               \n" +
-                "                                    ´´´`````````  ```  ¨¨´´´´´´`` ``` `````    `                    \n" +
-                "                                               ```     ´´·¨´       ```  ```                       ` \n" +
-                "                                                        ```´´´¨`` ``   ` `     `                `` `\n" +
-                "\n");
-        //https://www.asciiart.eu/image-to-ascii
-        System.out.println("Press any key");
-        anykey = scan.next();
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
-                "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-
-
-        System.out.println("you saw some black point in the sky of the coast.");
-        System.out.println("                                                   X                            \n" +
-                "           X X  X                         X   X                      X   X      \n" +
-                "         X                                       X                     X   X    \n" +
-                "             X   X                         X   X                                \n" +
-                "           X                                        X                           \n" +
-                "                                                                                \n" +
-                "                                                                                \n" +
-                "                                                                                \n" +
-                "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n" +
-                "                                                                                \n" +
-                "                                                                                \n" +
-                "XXXXXXXXXXXXXX                                                                  \n" +
-                "             XXXXXXXXXXX                                                        \n" +
-                "                       XXXXXXXX                                                 \n" +
-                "                              XXXXXXXXXXXXX                                     \n" +
-                "                                          XXXXXXXXXXXXXX                        \n" +
-                "XXXXX                                                  XXXXXXXXX                \n" +
-                "X   XXXXXXX                                                    XXXXX            \n" +
-                "          XXXXXXXX                                                 XXXXX        \n" +
-                "   XXXXX         XXXXXXXX                                              XXXXX    \n" +
-                "       XX               XXXXXXXXXX                                          XXXX\n" +
-                "     XXXXXXXXX                   XXXXXXXXX                                     X\n" +
-                " XXX   XXX   XXXX    X                    XXXXXXXXXXXXXXX                       \n" +
-                "XX XX    XXX    X    XXXX                               XXXXXXXXXXX XX          \n" +
-                "    XXX    XXXX         XXXX                                         XXX        ");
-        //https://www.asciiart.eu/ascii-draw-studio/app
-        System.out.println("\nBefore this mission, your assistant told you British and Germany are in war, \n" +
-                "but you look down upon. Now you treat this as a truth. You ask your copilot take a picture, \n" +
-                "");
-        System.out.println("Press any key");
-        anykey=scan.next();
-
-
-        if ((process % 100) % 10 == 0)
-            process = process + 1;
-
+        while ((process%100)%10==2&&quit.equals("y")) {
+            System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n" +
+                    "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+            System.out.println("you saw some black point in the sky of the coast.");
+            System.out.println("                                                   X                            \n" +
+                    "           X X  X                         X   X                      X   X      \n" +
+                    "         X                                       X                     X   X    \n" +
+                    "             X   X                         X   X                                \n" +
+                    "           X                                        X                           \n" +
+                    "                                                                                \n" +
+                    "                                                                                \n" +
+                    "                                                                                \n" +
+                    "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX\n" +
+                    "                                                                                \n" +
+                    "                                                                                \n" +
+                    "XXXXXXXXXXXXXX                                                                  \n" +
+                    "             XXXXXXXXXXX                                                        \n" +
+                    "                       XXXXXXXX                                                 \n" +
+                    "                              XXXXXXXXXXXXX                                     \n" +
+                    "                                          XXXXXXXXXXXXXX                        \n" +
+                    "XXXXX                                                  XXXXXXXXX                \n" +
+                    "X   XXXXXXX                                                    XXXXX            \n" +
+                    "          XXXXXXXX                                                 XXXXX        \n" +
+                    "   XXXXX         XXXXXXXX                                              XXXXX    \n" +
+                    "       XX               XXXXXXXXXX                                          XXXX\n" +
+                    "     XXXXXXXXX                   XXXXXXXXX                                     X\n" +
+                    " XXX   XXX   XXXX    X                    XXXXXXXXXXXXXXX                       \n" +
+                    "XX XX    XXX    X    XXXX                               XXXXXXXXXXX XX          \n" +
+                    "    XXX    XXXX         XXXX                                         XXX        ");
+            //https://www.asciiart.eu/ascii-draw-studio/app
+            System.out.println("\nBefore this mission, your assistant told you British and Germany are in war, \n" +
+                    "but you look down upon. Now you treat this as a truth. You ask your copilot take a picture, \n");
+            System.out.println("Press any key");
+            anykey = scan.next();
+            if (anykey.equals("q")||anykey.equals("Q"))
+                quit="n";
+            else
+                process = process + 1;
+        }
     }
 
 
@@ -112,8 +140,7 @@ public class RPG_Project {
 
         System.out.println("\nThis is task two.");
 
-
-        if (process%100<10)
+        if (process%100==3)
             process=process+10;
     }
 
@@ -122,8 +149,7 @@ public class RPG_Project {
 
 
         System.out.println("\nThis is the task three.");
-
-        if (process<100)
+        if (process%100==13)
             process=process+100;
     }
 
@@ -147,20 +173,21 @@ public class RPG_Project {
 
         System.out.println("1.");
         System.out.println("2.Eagle Day");
-        if ((process % 100) % 10 == 0)
+        if (process % 100<3)
             System.out.println(" (Not available now)\n");
         System.out.println("3.Aftermath");
         if (process % 100 < 10)
             System.out.println(" (Not available now)\n");
         System.out.println("\n\n5.Save the game"+"   6.Enter the game code"+"   7.Test mode(all task will be available)" +
                 "\n8.Exit the game");
-        System.out.println("select the task and enter its number");
+        System.out.println("select the task and enter its number\nRemember, in all any key step, press q or Q to quit " +
+                "the task and back to menu.");
 
         int select = scan.nextInt();
         if (select == 1)
             task1(scan);
         else if (select == 2) {
-            if ((process % 100) % 10 == 0) {
+            if (process % 100<3){
                 System.out.println("Not able, finish task1 to make it available.");
                 System.out.println("Press any key");
                 anykey = scan.next();
